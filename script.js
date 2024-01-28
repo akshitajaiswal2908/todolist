4f7682cc18429c2d6cf2cd104e2ed7f1e93f60aa
akshitajaiswal2908 <akshitajaiswal2908@gmail.com>
2024-01-29 03:12:54 +0530

    adding delete button

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+ 
+
+
 console.log("hi")
 var add = document.getElementById("buttonadd")
 
 function adding(){
     var task=document.getElementById("taskdata").value
+
     var lists=document.getElementById("list")
+
     var newnode=document.createElement("li")
     newnode.innerHTML=task;
-   lists.appendChild(newnode)
-   document.getElementById("taskdata").value=null;
+    var button=document.createElement("button")
+    button.textContent="DONE";
+    button.addEventListener('click',deleting)
+    newnode.appendChild(button);
+    lists.appendChild(newnode)
+
+
+
+    document.getElementById("taskdata").value=null;
     
 }
+function deleting(e)
+{  
+    var li =e.target.parentNode;
+    li.remove();
+
+}
+
 add.addEventListener('click',adding)
+ 
